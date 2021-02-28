24b7b4b369172d17d075bbe3748e9b3ceb67c1ee
lixj-zj <lixj_zj@163.com>
2021-02-28 18:51:13 +0800

    更新SQL；

--- springboot-file/src/main/resources/sql/springboot-demos.sql
+++ springboot-file/src/main/resources/sql/springboot-demos.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 25/02/2021 23:04:06
+ Date: 28/02/2021 18:49:42
 */
 
 SET NAMES utf8mb4;
@@ -22,21 +22,23 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `excel_info`;
 CREATE TABLE `excel_info`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `sex` int(1) NULL DEFAULT NULL,
-  `borrow_num` decimal(10, 0) NULL DEFAULT NULL,
-  `password` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `borrow_num` decimal(20, 0) NULL DEFAULT NULL,
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `create_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
-  `note` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `note` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = 'Excel文件demo' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = 'Excel文件demo' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of excel_info
 -- ----------------------------
-INSERT INTO `excel_info` VALUES (1, '张三', 1, 1000002, 'abcd1234!@#', '2021-02-25 23:02:12', '2021-02-25 23:03:40', NULL);
-INSERT INTO `excel_info` VALUES (2, '李四', 2, 489132132, 'jlsajkg', '2021-02-25 23:02:33', '2021-02-25 23:03:49', NULL);
+INSERT INTO `excel_info` VALUES (14, '张三', 1, 123456789, 'test!@#', '2021-02-28 18:46:02', '2021-02-28 18:46:02', '435');
+INSERT INTO `excel_info` VALUES (15, '李四', 0, 841315131225, 'ucosang#@%', '2021-02-28 18:46:02', '2021-02-28 18:46:02', '798');
+INSERT INTO `excel_info` VALUES (16, '王五', 1, 123456789, 'test!@#', '2021-02-28 18:46:02', '2021-02-28 18:46:02', '123');
+INSERT INTO `excel_info` VALUES (17, '赵六', 0, 841315131225, 'ucosang#@%', '2021-02-28 18:46:02', '2021-02-28 18:46:02', '234');
 
 SET FOREIGN_KEY_CHECKS = 1;
