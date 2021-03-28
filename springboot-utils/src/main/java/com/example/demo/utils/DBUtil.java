b624a2597e232efdba719748aea4f0624b5784c3
lixj-zj <lixj_zj@163.com>
2021-03-28 17:12:32 +0800

    utils-dbutil 更新

--- springboot-utils/src/main/java/com/example/demo/utils/DBUtil.java
+++ springboot-utils/src/main/java/com/example/demo/utils/DBUtil.java
@@ -3,29 +3,51 @@ package com.example.demo.utils;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Properties;
 
 public class DBUtil {
-	
-	private static Properties props;
-	
-	static {
-		try {
-			InputStream in = DBUtil.class.getClassLoader().getResourceAsStream("application.properties");
-			props = new Properties();
-			props.load(in);
-			in.close();
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static Connection getConn() throws Exception{
-		String url = props.getProperty("spring.datasource.url");
-		String username = props.getProperty("spring.datasource.username");
-		String password = props.getProperty("spring.datasource.password");
-		String driver = props.getProperty("spring.datasource.driver-class-name");
-		Class.forName(driver);
-		return DriverManager.getConnection(url,username, password);
-	}
+
+    private static Properties props;
+
+    static {
+        try {
+            InputStream in = DBUtil.class.getClassLoader().getResourceAsStream("application-dev.yml");
+            props = new Properties();
+            props.load(in);
+            if (null != in) {
+                in.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConn() throws Exception {
+        String url = props.getProperty("url");
+        String username = props.getProperty("username");
+        String password = props.getProperty("password");
+        String driver = props.getProperty("driver-class-name");
+        Class.forName(driver);
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Connection conn = DBUtil.getConn();
+            String sql = "SELECT * FROM salaries WHERE emp_no = ?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setInt(1, 10001);
+
+            ResultSet result = statement.executeQuery();
+            System.out.println(result);
+
+            statement.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
