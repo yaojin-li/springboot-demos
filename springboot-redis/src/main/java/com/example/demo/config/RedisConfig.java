41a3636a53deb7991785c5c950dc63a32e37423a
lixj-zj <lixj_zj@163.com>
2021-05-11 17:12:56 +0800

    代码优化；

--- springboot-redis/src/main/java/com/example/demo/config/RedisConfig.java
+++ springboot-redis/src/main/java/com/example/demo/config/RedisConfig.java
@@ -1,19 +1,13 @@
 package com.example.demo.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.util.StringUtils;
-import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * @Description: --------------------------------------
