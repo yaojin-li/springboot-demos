a098006946cf6f5ddb2820bd0e41120ad5f5847d
lxj <1549148883@qq.com>
2021-12-27 09:20:37 +0800

    redis pipeline

--- springboot-redis/src/main/java/com/example/demo/controller/PipelineController.java
+++ springboot-redis/src/main/java/com/example/demo/controller/PipelineController.java
@@ -1,11 +1,24 @@
 package com.example.demo.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisClusterConnection;
+import org.springframework.data.redis.connection.RedisClusterNode;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.StringRedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisConnectionUtils;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.web.bind.annotation.RestController;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.Pipeline;
+import redis.clients.jedis.*;
+import redis.clients.jedis.util.JedisClusterCRC16;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Description: 测试redis pipeline
@@ -20,53 +33,106 @@ import java.util.Date;
 @RestController
 @SuppressWarnings("all")
 public class PipelineController {
+    @Autowired
+    private RedisTemplate redisTemplate;
 
-    public static void main(String[] args) {
-        // 连接池
-        JedisPool jedisPool = new JedisPool("127.0.0.1", 6379);
-        Jedis jedis = null;
+//    @Scheduled(cron = "0 */1 * * * ?")
+    public void test() {
+        List<String> keys = new ArrayList<>();
+        keys.add("Single1");
+        keys.add("Single2");
+        keys.add("Single3");
+        System.out.println(JSONObject.toJSONString(multiGet(keys)));
+        System.out.println(JSONObject.toJSONString(hgetpipeline(keys)));
+    }
 
+    /**
+     * 批量查询
+     * 只建立一次连接批量查询，减少IO
+     */
+    public List<String> multiGet(List<String> keys) {
+        return redisTemplate.opsForValue().multiGet(keys);
+    }
+
+    /**
+     * 批量查询
+     * 只建立一次连接批量查询，减少IO
+     */
+    public List<Object> hgetpipeline(List<String> keys) {
+        List<Object> results = redisTemplate.executePipelined(
+                new RedisCallback<Object>() {
+                    @Override
+                    public Object doInRedis(RedisConnection connection) throws DataAccessException {
+                        StringRedisConnection stringRedisConn = (StringRedisConnection) connection;
+                        for (String key : keys) {
+                            stringRedisConn.get(key);
+                        }
+                        return null;
+                    }
+                });
+        return results;
+    }
+
+
+    public List<String> clusterPiplineGet(List<String> keys) {
+        RedisSerializer keySerializer = redisTemplate.getKeySerializer();
+        RedisSerializer<String> valueSerializer = redisTemplate.getValueSerializer();
+        // 记录节点与key之间的对应关系
+        HashMap<RedisClusterNode, List<String>> nodeKeyMap = new HashMap<>();
+        List<String> result = new ArrayList<>();
+        RedisClusterConnection redisClusterConnection = redisTemplate.getConnectionFactory().getClusterConnection();
         try {
-            jedis = jedisPool.getResource();
-            TimeLag t = new TimeLag();
-            Pipeline pipelined = jedis.pipelined();
-
-            // 插入多条数据
-            for (Integer i = 0; i < 100000; i++) {
-                pipelined.set(i.toString(), i.toString());
+            //通过计算每个key的槽点，并获取相应节点
+            Iterable<RedisClusterNode> redisClusterNodes = redisClusterConnection.clusterGetNodes();
+            for (RedisClusterNode redisClusterNode : redisClusterNodes) {
+                RedisClusterNode.SlotRange slotRange = redisClusterNode.getSlotRange();
+                for (String key : keys) {
+                    int slot = JedisClusterCRC16.getSlot(key);
+                    if (slotRange.contains(slot)) {
+                        List<String> list = nodeKeyMap.get(redisClusterNode);
+                        if (null == list) {
+                            list = new ArrayList<>();
+                            nodeKeyMap.putIfAbsent(redisClusterNode, list);
+                        }
+                        list.add(key);
+                    }
+                }
             }
+            for (Map.Entry<RedisClusterNode, List<String>> clusterNodeListEntry : nodeKeyMap.entrySet()) {
+                RedisClusterNode redisClusterNode = clusterNodeListEntry.getKey();
+                //连接节点
+                JedisPool jedisPool = ((JedisCluster) redisClusterConnection.getNativeConnection()).getClusterNodes().get(new HostAndPort(redisClusterNode.getHost(), redisClusterNode.getPort()).toString());
+                List<String> nodeListEntryValue = clusterNodeListEntry.getValue();
 
-//            // 删除多条数据
-//            for (Integer i = 0; i < 100000; i++) {
-//                pipelined.del(i.toString());
-//            }
-
-            pipelined.sync();
-            System.out.println(t.cost());
-        } catch (Exception e) {
-            e.printStackTrace();
+                byte[][] arr = new byte[nodeListEntryValue.size()][];
+                int count = 0;
+                //获取key数据
+                for (String nodeKey : nodeListEntryValue) {
+                    arr[count++] = keySerializer.serialize(nodeKey);
+                }
+                Jedis jedis = jedisPool.getResource();
+                List<Response<byte[]>> responses = new ArrayList<>();
+                try {
+                    Pipeline pipeline = jedis.pipelined();
+                    for (String nodeKey : nodeListEntryValue) {
+                        responses.add(pipeline.get(keySerializer.serialize(nodeKey)));
+                    }
+                    pipeline.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    jedis.close();
+                }
+                for (Response<byte[]> response : responses) {
+                    byte[] data = response.get();
+                    result.add(valueSerializer.deserialize(data));
+                }
+            }
         } finally {
-            if (jedis != null) {
-                jedis.close();
-            }
+            RedisConnectionUtils.releaseConnection(redisClusterConnection, redisTemplate.getConnectionFactory());
         }
-    }
-}
-
-
-class TimeLag {
-
-    private Date start;
-    private Date end;
-
-    public TimeLag() {
-        start = new Date();
+        return result;
     }
 
-    public String cost() {
-        end = new Date();
-        long c = end.getTime() - start.getTime();
-        return new StringBuffer().append("cost ").append(c).append(" milliseconds (").append(c / 1000).append(" seconds).").toString();
-    }
 
 }
