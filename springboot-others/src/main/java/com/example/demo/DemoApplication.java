a695b456b0b7af0c8f1191c88c88a13c3159eea3
lxj <1549148883@qq.com>
2021-12-29 17:39:07 +0800

    @scheduled注解多线程测试；

--- springboot-others/src/main/java/com/example/demo/DemoApplication.java
+++ springboot-others/src/main/java/com/example/demo/DemoApplication.java
@@ -5,8 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class DemoApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
